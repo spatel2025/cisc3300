f099393b7953c5729bd72e53fe2d32a03e423504
spatel2025 <spatel150@fordham.edu>
2024-01-29 18:15:41 -0500

    testfile

--- hw1.cpp
+++ hw1.cpp
@@ -1,1 +1,39 @@
-test file
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Me</title>
+    <style>
+        body { font-family: Arial, sans-serif; }
+        .interests, .education, .career-goals { margin-top: 20px; }
+    </style>
+</head>
+<body>
+
+    <h1>About Me</h1>
+
+    <h2>Introduction</h2>
+    <p>Hello! My name is Shreya. I am a psychology major and a computer science minor. </p>
+
+    <div class="education">
+        <h3>Education</h3>
+        <p>After my undergrad at Fordham University, I will pursue grad school.  I expect to graduate from Fordham in the spring of 2025.</p>
+    </div>
+
+    <div class="career-goals">
+        <h3>Career Goals</h3>
+        <p>After completing my degree, my goal is to become a clinical psycholgist and obtain a PsYD.  My main focus is to work in a hospital setting treating behavioral and developmental disorders.</p>
+    </div>
+
+    <div class="interests">
+        <h3>Hobbies and Interests</h3>
+        <p>In my free time, I enjoy playing tennis, hanging out with friends, traveling, and listening to music.</p>
+    </div>
+
+    <div class="Family">
+        <h3>Home</h3>
+        <p>I live in Los Angeles, California, and have three younger siblings.</p>
+    </div>
+
+</body>
+</html>
