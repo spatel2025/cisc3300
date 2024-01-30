4296c52879785b1cae9ef829e9f16e49d813e7f7
spatel2025 <spatel150@fordham.edu>
2024-01-29 19:09:57 -0500

    hw1pt2

---
+++ hw#1pt2.cpp
@@ -0,0 +1,20 @@
+1. What happens from the time a user puts a URL into their browser to when they are able to view a webpage? - Briefly define ISP, URL, DNS, web server.
+
+When a user puts a URL into their browser, it simply gives the browser an address.  For instance, the browser requests the ISP how to find the address.  Then the ISP checks the DNS to find the exact IP address.  This IP address is where the website is located (GPS).  After this, the browser then sends a request to the server of the website for the previous IP address that was used.  Lastly, the server will send the page of the website back and then the user is able to view the webpage.  ISP is the internet service provider which gives internet access to all users to be able to connect to the internet.  THE URL is the uniform resource locator which is the address of a particular resource on the web.  For example, it contains the “http”.  THE DNS is the domain name system which translates a url or domain name into a certain IP address so computers can detect it and find it on the internet.  A web server is what stores all the websites and gives users access to view them on the internet.
+
+
+2. What is html? What does it do?
+
+HTML is the hypertext markup language in which users use this language to create web pages.  It created content on a webpage depending on what the user includes.  For instance, it can include elements like paragraphs, titles, links, etc.  HTML requests the web browser to then display all elements used in the web page for users to view.
+
+
+3. Access https://www.fordham.edu/. In your own words, describe how this webpage uses structure to convey meaning and navigation options to the user.
+
+When viewing Fordham University’s website, the webpage uses structure to convey information and navigation options to the user.  For instance, it includes a side bar containing links to sections like about, mission, academics, admissions and aid, research, student life, resources, and athletics.  It also includes a homepage where it shows pictures of student life at the university and recent news.  Below the webpage it includes helpful links such as different policies and campus tours.  It also includes the University’s social media pages.
+
+
+4. What is an html element? Tag? Attribute?
+
+An HTML element is what is needed for any HTML document.  It is the foundation which includes content, as well as the start tag and end tag.  A tag is what marks the start tag and end tag of the certain HTML element.  It is also used to instruct the browser on how the content should be displayed or formatting protocols.  An attribute is what gives information about a specific element.  It lies in between a start tag and can be used with a pair such as name or value.
+
+
