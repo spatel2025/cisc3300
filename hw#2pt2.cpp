00857bb9e3ff0f1ea1468bd01a25c41085847bdb
spatel2025 <spatel150@fordham.edu>
2024-01-29 19:19:05 -0500

    hw2pt2

---
+++ hw#2pt2.cpp
@@ -0,0 +1,19 @@
+1. Define structural vs semantic meaning in regard to HTML elements.
+
+In regard to HTML, structural meaning refers to how the elements of a particular website are displayed.  For instance, it illustrates how sidebars, headers, etc. are organized.  Semantic meaning has to do with the purpose of how elements are used.  It uses a name in between < > to describe the content and it’s purpose for this specific element.
+
+
+2. Why is it important to use correct html elements when designing web pages?
+
+It is important to use correct HTML elements when designing web pages because it allows for the webpage to be easily accessible to users and display organized structure.  It can also help browsers know what content to prioritize in the web page for the user to have an optimal experience in viewing the webpage.
+
+
+3. What are forms in html? What is their purpose? When would you use one?
+
+Forms in HTML is used to collect a user’s input.  For instance, it can be seen as submission buttons, text fields, etc.  The purpose of forms is so that users can log into the website, submit information, or even search the website.  It simply allows for user interaction.  You would use a form in HTML to create a search bar, a survey online, or a login webpage.
+
+
+4. Define and contrast inline vs block elements.
+
+Block elements are elements that start on a new line and take up the width that is left, it creates structures that are large.  A block element example is <p> or <div>.  An inline element doesn’t begin on a new line, and takes up the width that is necessary for the line, it is used in the flow of content.  An inline element example is <a>.  
+
