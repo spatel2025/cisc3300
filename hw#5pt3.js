1704283b26a8781e089ec995e0f10bce6b4ea2a6
spatel2025 <spatel150@fordham.edu>
2024-02-26 18:04:42 -0500

    hw5pt3

--- hw#5pt3.js
+++ hw#5pt3.js
@@ -1,4 +1,4 @@
 
-onst numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5];
 const squaredNumbers = numbers.map(number => number * number);
 console.log(squaredNumbers); // Output will be [1, 4, 9, 16, 25]
